4a95b310f1723111fe2cdff2081bbd0f43da4993
Kiril Zyapkov <ziapkov@gmail.com>
2023-10-03 22:07:59 +0300

    Seting up the pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JobSearchComponent } from './pages/job-search/job-search.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { JobseekerComponent } from './pages/registration/components/jobseeker/jobseeker.component';
+import { EmployerComponent } from './pages/registration/components/employer/employer.component';
+import { JobDetailsComponent } from './pages/job-details/job-details.component';
+import { MyProfileComponent } from './pages/my-profile/my-profile.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobSearchComponent,
+    LoginComponent,
+    RegistrationComponent,
+    JobseekerComponent,
+    EmployerComponent,
+    JobDetailsComponent,
+    MyProfileComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
