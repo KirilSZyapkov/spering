e0156aa5ae3495804d6f9e8c1b5053429b930ebf
Kiril Zyapkov <ziapkov@gmail.com>
2023-11-05 19:45:12 +0200

    Styling the  profile page.

--- src/app/pages/my-profile/my-profile.component.ts
+++ src/app/pages/my-profile/my-profile.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserContextService } from 'src/app/context/user-context.service';
+import { User } from 'src/interface/User';
 
 @Component({
   selector: 'app-my-profile',
   templateUrl: './my-profile.component.html',
-  styleUrls: ['./my-profile.component.css']
+  styleUrls: ['./my-profile.component.css'],
 })
-export class MyProfileComponent {
+export class MyProfileComponent implements OnInit {
+  myProfile: User | null;
 
+  constructor(private useContext: UserContextService) {}
+
+  ngOnInit(): void {
+    this.useContext.value.subscribe((acc) => (this.myProfile = acc));
+    console.log(this.myProfile);
+    
+  }
 }
