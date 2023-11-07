50916205acab196cb4dc0b1a26fb2ea5092ce42a
Kiril Zyapkov <ziapkov@gmail.com>
2023-11-07 22:08:13 +0200

    Fetching the details of a job.

--- src/app/pages/job-details/job-details.component.ts
+++ src/app/pages/job-details/job-details.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { JOBS_POSTS } from 'src/constants';
+import { Job } from 'src/interface/Job';
 
 @Component({
   selector: 'app-job-details',
   templateUrl: './job-details.component.html',
-  styleUrls: ['./job-details.component.css']
+  styleUrls: ['./job-details.component.css'],
 })
-export class JobDetailsComponent {
+export class JobDetailsComponent implements OnInit {
+  jobDetail: Job;
+  id: string;
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get("id");
+    this.jobDetail = JOBS_POSTS.find(j => j.id === Number(this.id));
+    console.log(this.jobDetail);
+    
+    console.log(this.id);
+    
+  }
 }
