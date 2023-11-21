8773813cf3a19beb910ed906c4f8e7c4fdedc94f
Kiril Zyapkov <ziapkov@gmail.com>
2023-11-21 21:24:17 +0200

    Styling the post job page.

--- src/app/pages/post-job/post-job.component.ts
+++ src/app/pages/post-job/post-job.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserContextService } from 'src/app/context/user-context.service';
 
 @Component({
   selector: 'app-post-job',
   templateUrl: './post-job.component.html',
-  styleUrls: ['./post-job.component.css']
+  styleUrls: ['./post-job.component.css'],
 })
 export class PostJobComponent {
+  postJobForm = new FormGroup({
+    jobTitle: new FormControl(''),
+    responsibilities: new FormControl(''),
+    forUs: new FormControl(''),
+    location: new FormControl(''),
+    weOffer: new FormControl(''),
+    necessarySkills: new FormControl(''),
+    contactInformation: new FormControl(''),
+    deadLine: new FormControl(''),
+    work: new FormControl(''),
+    schedule: new FormControl(''),
+    suitableFor: new FormControl(''),
+    experience: new FormControl(''),
+    salary: new FormControl(''),
+    logoUrl: new FormControl(''),
+  });
 
+  constructor(private useContext: UserContextService, private router: Router){}
 }
