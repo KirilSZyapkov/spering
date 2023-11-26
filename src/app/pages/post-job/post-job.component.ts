b4ef14a42a801cb7b0b2d46e023d560a1871c147
Kiril Zyapkov <ziapkov@gmail.com>
2023-11-26 22:29:42 +0200

    Adding guard routs for post jobs.

--- src/app/pages/post-job/post-job.component.ts
+++ src/app/pages/post-job/post-job.component.ts
@@ -34,9 +34,17 @@ export class PostJobComponent implements OnInit{
  ngOnInit(): void{
   this.useContext.value.subscribe((user)=> this.user = user);
   if(this.user.firstName === undefined){
+    alert("You have to login as employer to post a job!");
     this.router.navigate(["login"]);
+  } else if (this.user.role !== "employer"){
+    if(this.user.firstName === undefined){
+      alert("You must registrate as employer!");
+      this.router.navigate(["registration"]);
+    } else {
+      alert("You must registrate as employer, logout first!");
+      this.router.navigate(["/"]);
+    }
   }
-  console.log();
  }
 
   
@@ -58,7 +66,11 @@ export class PostJobComponent implements OnInit{
       logoUrl,
     } = this.postJobForm.controls;
     try {
-      const newPost = {};
+      const newPost = {
+        id: Math.floor(Math.random() * 10000000000).toString(36),
+        posted_at: new Date(),
+        posted_from: this.user.companyName,
+      };
       alert('hi');
     } catch (error) {
       alert(error.message);
